6970772766abc276f0ce0064ff8bdd9c72ccaf88
David Vilchez <dvilchez@xuaps.com>
2013-07-10 15:19:52 +0000

    grader and check updates

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +36,12 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUriExists = function(urifile) {
+
+
+
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -61,14 +68,31 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		  .option('-u, --url <uri_file>', 'Uri to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	if(program.url){
+			rest.get(program.url).on('complete', function(result) {
+			  if (result instanceof Error) {
+					console.log('%s does not exist. Exiting', result);
+					process.exit(1);
+			  } else {
+				fs.writeFileSync('index.html', result);
+		   	var checkJson = checkHtmlFile(program.file, program.checks);
+			   var outJson = JSON.stringify(checkJson, null, 4);
+			   console.log(outJson);
+			}
+		});
+	}else{
+		var checkJson = checkHtmlFile(program.file, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	};
+	
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
